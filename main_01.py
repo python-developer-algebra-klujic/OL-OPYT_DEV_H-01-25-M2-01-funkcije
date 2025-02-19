375beea56cf1c76c7c54e9e74e21bf590a087c2a
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-19 20:22:08 +0100

    Create main_01.py

---
+++ main_01.py
@@ -0,0 +1,44 @@
+
+
+# Deklaracija funkcije welcome
+# kljucna rijec def, yatimnayiv funkcije koji mi odredujemo
+# te argumenti u obicnim zagradama i nakon toga : i tijelo funkcije
+def welcome(name: str) -> None:
+    '''
+    Funkcija welcome ocekuje agrument name za ime osobe
+    za koju ce ispisati pozdravnu poruku
+    '''
+    print()
+    print(f'Pozdrav, {name}!')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# "Poziv funkcije", odnosno pokretanje funkcije welcome()
+welcome('Iva Ivic')
